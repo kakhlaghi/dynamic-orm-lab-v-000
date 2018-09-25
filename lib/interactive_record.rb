b84fafe4fda9ce0707d58dc0c598db545feb6a63
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-25 16:36:49 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
   
   def self.find_by(attribute_hash)
     attribute = attribute_hash.values.first
-    sql = "SELECT * FROM students WHERE name = attribute"
+    sql = "SELECT * FROM students WHERE name = '#{attribute}'"
         DB[:conn].execute(sql)
   end
 

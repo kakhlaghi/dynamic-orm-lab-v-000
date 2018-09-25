171cbec1d6652bd2d73f34f85d26851a8929d829
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-25 15:34:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,9 +7,9 @@ class Student < InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
-  def self.column_names
+  
    self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
-  end
+  
   
 end
